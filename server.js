3fc36a409e397b3718da6ee7a08cea573e732024
Novo1999 <neptune.navo@gmail.com>
2024-06-07 22:04:37 +0600

    added: base url

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ const server = createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: process.env.BASE_URL,
     methods: ['GET', 'POST'],
   },
 })
